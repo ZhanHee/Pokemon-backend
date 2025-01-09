fa84b2e6872f601a4f4d30063820997c8e640c5f
ZhanHee <zhangzhanhe66@gmail.com>
2025-01-09 17:51:03 +0100

    pipline

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const connectDB = require('./config/database');
+const pokemonRoutes = require('./routes/pokemon');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(bodyParser.json());
+
+// 路由
+app.use('/api/pokemons', pokemonRoutes);
+
+//front et backend
+const cors = require('cors');
+app.use(cors());
+
+// 启动服务器
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
